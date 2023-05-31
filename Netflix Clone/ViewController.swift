66676f7bbb0338e61a70fba4b5d647735cc6d905
saminshakya <saminshakya27@gmail.com>
2023-05-31 09:56:12 +0545

    project setup

---
+++ Netflix Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by MicroBanker Nepal Pvt. Ltd. on 31/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
