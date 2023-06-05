a1a9c403472565bdeaab02aa31a2263e4520a1a8
saminshakya <saminshakya27@gmail.com>
2023-06-05 15:47:12 +0545

    added title to upcoming tab bar

---
+++ Netflix Clone/Views/TitleTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  TitleTableViewCell.swift
+//  Netflix Clone
+//
+//  Created by MicroBanker Nepal Pvt. Ltd. on 05/06/2023.
+//
+
+import UIKit
+
+class TitleTableViewCell: UITableViewCell {
+
+ 
+
+}
