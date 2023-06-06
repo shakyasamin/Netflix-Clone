a058e308e22fe5c3aacdd843c437417ee6b16473
saminshakya <saminshakya27@gmail.com>
2023-06-06 15:27:45 +0545

    search functionality completed

--- Netflix Clone/Controllers/Core/SearchViewController.swift
+++ Netflix Clone/Controllers/Core/SearchViewController.swift
@@ -43,6 +43,8 @@ class SearchViewController: UIViewController {
         
         fetchDiscoverMovies()
         
+        searchController.searchResultsUpdater = self
+        
     }
     
     private func fetchDiscoverMovies() {
@@ -84,4 +86,25 @@ extension SearchViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 140
     }
+}
+
+extension SearchViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        let searchBar = searchController.searchBar
+        
+        guard let query = searchBar.text,!query.trimmingCharacters(in: .whitespaces).isEmpty,query.trimmingCharacters(in: .whitespaces).count >= 3,
+              let resultsController = searchController.searchResultsController as? SearchResultsViewController else {return}
+        Apicaller.shared.search(with: query) { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let titles):
+                    resultsController.titles = titles
+                    resultsController.searchResultsCollectionView.reloadData()
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+
 }
