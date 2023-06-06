6edb3f25534b998252e3b4fe8ea25f97c7c7aeac
saminshakya <saminshakya27@gmail.com>
2023-06-06 11:43:22 +0545

    added title image and playbutton to searchVC

---
+++ Netflix Clone/Controllers/SearchResultsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchResultsViewController.swift
+//  Netflix Clone
+//
+//  Created by MicroBanker Nepal Pvt. Ltd. on 06/06/2023.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
